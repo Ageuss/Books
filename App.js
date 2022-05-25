2f60bd280ed9617a98addfb6f91f845460fab2b9
ageuss <ageuoliveira0@gmail.com>
2022-05-25 20:33:41 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
+import { SafeAreaView, Text, StyleSheet, View, FlatList, TextInput, Button} from 'react-native';
+import api from './api';
+
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [filtered, setFiltered] = useState([]);
+
+  const filter = (fieldText) => {
+    if (fieldText) {
+      api.get(fieldText).then((Response) => {
+        for(var i = 0; i < Response.data.hits.length; i++){
+          if(Response.data.hits){
+            setFiltered(Response.data.hits);
+          }
+        }
+        setSearch(fieldText);
+      });
+    }
+  };
+
+  const ViewItems = ({item}) => {
+    return (
+      <Text style={styles.items}>
+        {'Author: '}
+        {item.author + '\n'}
+        {'Title: '}
+        {item.title + '\n'}
+        {'Url: '}
+        {item.url}
+      </Text>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>Books</Text>
+        <TextInput
+          style={styles.inpt}
+          onChangeText = {(fieldText) => setSearch(fieldText)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Buscar"
+        />
+        <Button
+        onPress = {() => filter(search)}
+        title="Buscar"
+        color="green"
+        />
+        <FlatList
+          data={filtered}
+          renderItem={ViewItems}
+        />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    backgroundColor: 'white',
   },
+  text: {
+    textAlign: "center",
+    fontSize: '150%',
+    marginBottom: '4%'
+  },
+  items: {
+    backgroundColor: '#0066CC',
+    padding: 8,
+    marginTop: 8,
+    color: 'white',
+  },
+  inpt: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+  },
+
 });
+
+export default App;
